758093c2bb01e99850d3705e8e51c671a19f93ef
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-17 00:38:06 +0200

    2 + 3 = 5

---
+++ src/com/jkay/task/level01/task06/Task02.java
@@ -0,0 +1,23 @@
+package com.jkay.task.level01.task06;
+
+/**
+ * 2 + 3 = 5
+ */
+
+public class Task02 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+        // System.out.print("два");
+        System.out.print(b);
+        System.out.print(" плюс ");
+        // System.out.print(" минус ");
+        System.out.print(a);
+        // System.out.print("три");
+        System.out.print(" равно ");
+        // System.out.print(" будет ");
+        // System.out.print("пять");
+        System.out.print(a + b);
+    }
+}
