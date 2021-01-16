908f04499c03306d3a1c61a3b4886d8e13e3a87f
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-16 23:57:06 +0200

    Task 3

---
+++ src/com/jkay/task/task01/task03/Solution.java
@@ -0,0 +1,13 @@
+package com.jkay.task.task01.task03;
+
+/**
+ *Объявляем переменные
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        String name;
+        String city;
+        int age;
+    }
+}
