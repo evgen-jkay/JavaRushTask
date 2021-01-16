8b403341991a74612caea828913b19f72b0a5c74
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-16 23:30:12 +0200

    task 1

---
+++ src/com/jkay/Main.java
@@ -0,0 +1,8 @@
+package com.jkay;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/jkay/task/task01/Solution.java
@@ -0,0 +1,14 @@
+package com.jkay.task.task01;
+
+/**
+ * Амиго очень умный
+ *
+ * Напиши программу, которая выводит на экран надпись: "Амиго очень умный".
+ *
+ */
+
+public class Solution {
+    public Solution() {
+        System.out.println("Амиго очень умный");
+    }
+}
