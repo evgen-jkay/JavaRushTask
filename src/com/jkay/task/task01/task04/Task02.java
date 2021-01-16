f333f6a39870574b4119838f4f0bff4c2997b07b
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-17 00:07:01 +0200

    Task 5

---
+++ src/com/jkay/task/task01/task04/Task02.java
@@ -0,0 +1,14 @@
+package com.jkay.task.task01.task04;
+
+/**
+ * Мой юный друг
+ */
+
+public class Task02 {
+    public static void main(String[] args) {
+        int currentYear = 3126;
+        int friendBirthday = currentYear - 8;
+        System.out.println(friendBirthday);
+    }
+}
+
