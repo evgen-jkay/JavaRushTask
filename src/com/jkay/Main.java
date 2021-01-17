9691e6beba1216866eefe0355e73e978fd532e2f
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-17 20:17:43 +0200

    LVL 3. Lecture 1. Task 1

--- src/com/jkay/Main.java
+++ src/com/jkay/Main.java
@@ -1,10 +1,8 @@
 package com.jkay;
 
 public class Main {
-
     public static void main(String[] args) {
-	    // write your code here
-        System.out.println(5/2);
-
+        
     }
 }
+
---
+++ src/com/jkay/task/level03/lecture01/Task01.java
@@ -0,0 +1,23 @@
+package com.jkay.task.level03.lecture01;
+
+/*
+Тепло или холодно
+
+Ввести с клавиатуры температуру на улице.
+Если температура меньше 0, вывести надпись "на улице холодно", иначе - вывести надпись "на улице тепло".
+*/
+
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int temperature = scanner.nextInt();
+
+        if (temperature < 0) {
+            System.out.println("на улице холодно");
+        } else {
+            System.out.println("на улице тепло");
+        }
+    }
+}
