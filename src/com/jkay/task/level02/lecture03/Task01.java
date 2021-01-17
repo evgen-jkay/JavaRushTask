9168591034370926442572ece89e9ed5a6e65d32
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-17 12:41:57 +0200

    Lecture 3. Task 1

---
+++ src/com/jkay/task/level02/lecture03/Task01.java
@@ -0,0 +1,19 @@
+package com.jkay.task.level02.lecture03;
+
+/*
+Логотип решает все
+
+В переменную tagline нужно записать строку "JustDoIt". Используй s1, s2, s3 и конкатенацию строк.
+*/
+
+public class Task01 {
+    public static void main(String[] args) {
+        String s1 = "Do";
+        String s2 = "It";
+        String s3 = "Just";
+
+        String tagline = s3 + s1 + s2;
+
+        System.out.println(tagline);
+    }
+}
