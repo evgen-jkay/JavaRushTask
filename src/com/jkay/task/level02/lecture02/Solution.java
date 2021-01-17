4e384e463ac439c8100b0c7e79f737ae50b6929a
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-17 02:53:03 +0200

    LVL 2. Lecture 2. Task 1

---
+++ src/com/jkay/task/level02/lecture02/Solution.java
@@ -0,0 +1,14 @@
+package com.jkay.task.level02.lecture02;
+
+/*
+«Я снова по объявлению…»
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String word = "word ",
+                phrase = "phrase ",
+                line = "line ",
+                text = "text";
+    }
+}
