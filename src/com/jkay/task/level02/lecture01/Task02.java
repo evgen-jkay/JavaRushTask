9c65b2b71cc4fcbf168f1418a2cfc3cdbb7b8ad8
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-17 02:17:19 +0200

    Lecture 2. Task 3

---
+++ src/com/jkay/task/level02/lecture01/Task02.java
@@ -0,0 +1,9 @@
+package com.jkay.task.level02.lecture01;
+
+public class Task02 {
+    public static void main(String[] args) {
+        int number = 546;
+        int lastDigit = number % 10;
+        System.out.println(lastDigit);
+    }
+}
