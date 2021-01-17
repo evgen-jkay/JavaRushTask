9355deaf1248e1a16f2ce52d4cd4e39af1efea29
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-17 02:13:48 +0200

    Lecture 2. Task 2

---
+++ src/com/jkay/task/level02/lecture01/Task01.java
@@ -0,0 +1,13 @@
+package com.jkay.task.level02.lecture01;
+
+/*
+Полнометражная картина
+*/
+
+public class Task01 {
+    public static void main(String[] args) {
+        int centimeters = 243;
+        int meters = centimeters / 100;
+        System.out.println(meters);
+    }
+}
