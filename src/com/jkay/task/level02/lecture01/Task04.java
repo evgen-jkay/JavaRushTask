fb72ccba4e6115a3f051c66c4dcec5dc7e1b31cf
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-17 02:38:42 +0200

    Lecture 2. Task 4

---
+++ src/com/jkay/task/level02/lecture01/Task04.java
@@ -0,0 +1,23 @@
+package com.jkay.task.level02.lecture01;
+
+/*
+Когда-то было 32 зуба
+*/
+
+public class Task04 {
+    public static void main(String[] args) {
+        int toothCounter = 32;
+
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+
+        System.out.println(toothCounter);
+    }
+}
