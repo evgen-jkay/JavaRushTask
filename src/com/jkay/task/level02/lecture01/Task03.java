a0f0385ce9b0318c39ef3704feea83070faa88ca
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-17 02:29:51 +0200

    Lecture 2. Task 3

---
+++ src/com/jkay/task/level02/lecture01/Task03.java
@@ -0,0 +1,17 @@
+package com.jkay.task.level02.lecture01;
+
+/*
+9-ка это перевернутая 6-ка
+*/
+
+public class Task03 {
+    public static void main(String[] args) {
+        int six = 6;
+
+        six++;
+        six++;
+        six++;
+
+        System.out.println(six);
+    }
+}
