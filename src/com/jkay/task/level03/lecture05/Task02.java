2cd3f96c0d743bae5d7c73cd51569c002c6308fd
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-18 21:36:16 +0200

    LVL 3. Lecture 5. Task 2 && 3

---
+++ src/com/jkay/task/level03/lecture05/Task02.java
@@ -0,0 +1,30 @@
+package com.jkay.task.level03.lecture05;
+
+/*
+Через тернии...
+
+Эта программа написана с использованием тернарного оператора, но не совсем понятно, что она делает.
+Думаем, если избавиться от тернарного оператора, то станет намного понятнее.
+Перепиши программу без использования тернарного оператора.
+*/
+
+import java.util.Scanner;
+
+public class Task02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        // String result = number < 5 ? "число меньше 5" : number > 5 ? "число больше 5" : "число равно 5";
+        String result;
+        if (number < 5) {
+            result = "число меньше 5";
+        } else if (number > 5) {
+            result = "число больше 5";
+        } else {
+            result = "число равно 5";
+        }
+
+        System.out.println(result);
+    }
+}
---
+++ src/com/jkay/task/level03/lecture05/Task03.java
@@ -0,0 +1,22 @@
+package com.jkay.task.level03.lecture05;
+/*
+Высокая точность
+
+Напиши программу, которая считывает с клавиатуры два вещественных числа и выдает сообщение о том,
+равны ли эти числа с точностью до одной миллионной.
+*/
+
+import java.util.Scanner;
+
+public class Task03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1 = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
+
+        if ( Math.abs(num2 - num1) < 0.000001 )
+            System.out.println("Числа равны");
+        else
+            System.out.println("Числа не равны");
+    }
+}
