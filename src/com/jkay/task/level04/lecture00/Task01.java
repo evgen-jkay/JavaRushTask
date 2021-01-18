64db2835915a515b61755466e1168ced23e6d25f
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-18 23:00:05 +0200

    LVL 4. Lecture 1. Task 1

---
+++ src/com/jkay/task/level04/lecture00/Task01.java
@@ -0,0 +1,20 @@
+package com.jkay.task.level04.lecture00;
+
+/*
+Я никогда не буду работать за копейки
+
+Используя цикл while вывести на экран сто раз цитату (переменная quote):
+«Я никогда не буду работать за копейки. Амиго»
+Каждое значение вывести с новой строки.
+*/
+
+public class Task01 {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(quote);
+        }
+    }
+}
