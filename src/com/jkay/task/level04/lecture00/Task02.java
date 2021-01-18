ef3e363f50ce5bbf43bc6a0812785e9531f7314b
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-18 23:06:59 +0200

    LVL 4. Lecture 1. Task 2

---
+++ src/com/jkay/task/level04/lecture00/Task02.java
@@ -0,0 +1,25 @@
+package com.jkay.task.level04.lecture00;
+
+/*
+Все любят Мамбу
+Ввести с клавиатуры имя и, используя цикл while, 10 раз вывести:
+<имя> любит меня (переменная text).
+Каждый вывод - с новой строки.
+*/
+
+import java.util.Scanner;
+
+public class Task02 {
+    public static void main(String[] args) {
+        String text = " любит меня.";
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(name + text);
+            i++;
+        }
+    }
+}
