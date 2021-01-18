5fb5821581a9ac6c1f977f7c92e1569e3f1fff2a
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-18 22:02:31 +0200

    LVL 3. Lecture 6. Task 2

---
+++ src/com/jkay/task/level03/lecture06/Task02.java
@@ -0,0 +1,33 @@
+package com.jkay.task.level03.lecture06;
+
+/*
+Сравнение строк по ссылке
+*/
+
+public class Task02 {
+    public static String string1 = "Амиго";
+    public static String string2 = string1;
+    public static String string3 = new String(string1);
+
+    public static void main(String[] args) {
+        String same = "ссылки на строки одинаковые";
+        String different = "ссылки на строки разные";
+        if (string1 == string2) {
+            System.out.println(same);
+        } else {
+            System.out.println(different);
+        }
+
+        if (string1 == string3) {
+            System.out.println(same);
+        } else {
+            System.out.println(different);
+        }
+
+        if (string2 == string3) {
+            System.out.println(same);
+        } else {
+            System.out.println(different);
+        }
+    }
+}
