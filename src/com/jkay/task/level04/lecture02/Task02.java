ea3d0d8e822c7f7ab938f44096e40a97baacac88
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-23 20:53:52 +0200

    LVL 4. Lecture 02. Task 02

--- src/com/jkay/task/level04/lecture02/Task02.java
+++ src/com/jkay/task/level04/lecture02/Task02.java
@@ -2,6 +2,15 @@ package com.jkay.task.level04.lecture02;
 
 import java.util.Scanner;
 
+/*
+Минимум из введенных чисел
+
+Программа должна считывать целые числа с клавиатуры до тех пор, пока не будет введено что-то другое:
+например, строка или символ.
+Выведи на экран минимальное число из введенных.
+Если введено несколько таких чисел, необходимо вывести любое из них.
+*/
+
 public class Task02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
