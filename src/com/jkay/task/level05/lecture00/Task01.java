028e322cd26044dbb5338bf973ba01de7c201784
evgen-jkay <evgenlandarenko@gmail.com>
2021-01-29 22:25:01 +0200

    В методе main проинициализируй переменные intArray и doubleArray массивами соответствующих типов, размер которых равен 10.

---
+++ src/com/jkay/task/level05/lecture00/Task01.java
@@ -0,0 +1,18 @@
+package com.jkay.task.level05.lecture00;
+
+/*
+Создаем массивы
+
+В методе main проинициализируй переменные intArray и doubleArray массивами соответствующих типов,
+размер которых равен 10.
+*/
+
+public class Task01 {
+    public static int[] intArray;
+    public static double[] doubleArray;
+
+    public static void main(String[] args) {
+        intArray = new int[10];
+        doubleArray = new double[10];
+    }
+}
